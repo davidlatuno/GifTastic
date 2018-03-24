369a763bb0d0fb8f3479430d250d53929f41a394
davidlatuno <dslatuno@gmail.com>
2018-03-23 21:33:34 -0700

    Changed gif append number to 10 and added active toggle jquery

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -6,7 +6,7 @@ for (var i = 0; i < moodArray.length; i++) {
     newButt.attr("type", "button");
     newButt.attr("class", "btn btn-default");
     newButt.attr("data-name", moodArray[i]);
-    newButt.text(moodArray[i]);
+    newButt.text(moodArray[i].toUpperCase());
     $(".button-area").append(newButt);
 
 }
@@ -16,17 +16,53 @@ $(document).ready(function () {
     $(".button-area").on("click", ".btn", function () {
 
         var userQuery = $(this).attr("data-name");
-        var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=sd2K2qfUpMftQdTEu7WlnmCrlCnZAJB8&q=" + userQuery + "&limit=10&offset=0&rating=G&lang=en";
+        var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=sd2K2qfUpMftQdTEu7WlnmCrlCnZAJB8&q=" + userQuery + "&limit=10&offset=0&rating=PG&lang=en";
 
         $.ajax({ url: queryUrl, method: "GET" }).then(function (response) {
 
-            var gImage = response.data[0].images.fixed_height.url;
-            var newImg = $("<img>");
-            newImg.attr("src", gImage);
-            newImg.attr("alt", userQuery);
-            $(".gif-area").append(newImg);
+            for (var j = 0; j < 10; j++) {
+                // Response variables
+                var gImageActive = response.data[j].images.fixed_height.url;
+                var gImageStill = response.data[j].images.fixed_height_still.url;
+                var rating = response.data[j].rating.toUpperCase();
+                // Jquery newDiv
+                var newDiv = $("<div>")
+                newDiv.attr("class", "gif");
 
+                // Grab gif from response
+                var newImg = $("<img>");
+                newImg.attr("src", gImageStill);
+                newImg.attr("alt", userQuery);
+                newImg.attr("data-active", gImageActive);
+                newImg.attr("data-still", gImageStill);
+                newImg.attr("data-status", "still");
+                newDiv.append(newImg);
+
+                // Add Rating of gif
+                var newP = $("<p>");
+                newP.text("Rating: " + rating);
+                newDiv.append(newP);
+
+                // prepend to html
+                $(".gif-area").prepend(newDiv);
+            }
         })
+    })
+
+
+    // Toggle gifs between active and still
+    $(".gif-area").on("click", "img", function() {
+        var still = $(this).attr("data-still");
+        var active = $(this).attr("data-active");
+        var status= $(this).attr("data-status");
+
+        if (status === "still") {
+            $(this).attr("src", active);
+            $(this).attr("data-status", "active");
+        } else {
+            $(this).attr("src", still);
+            $(this).attr("data-status", "still");
+        }
 
 
 
